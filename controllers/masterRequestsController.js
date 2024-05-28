11697eb23483bc5f3989e2fdd401c1ce3cd9154a
gvorn <157733779+gvorn7@users.noreply.github.com>
2024-05-28 17:11:19 +0700

    v13

--- controllers/masterRequestsController.js
+++ controllers/masterRequestsController.js
@@ -1,26 +1,7 @@
 const { connectDb, closeDb, poolPromise } = require("../config/db.config");
 var Type = require("mssql").TYPES;
 
-  // Emp_Code,
-  //       Doc_No,
-  //       _Division,
-  //       case_,
-  //       dateOfReq,
-  //       OPIST_Process,
-  //       ITEM_NO,
-  //       OPIST_PartNo,
-  //       SPEC,
-  //       OPIST_Usage_pcs,
-  //       Revision,
-  //       OPIST_MC,
-  //       MCNo,
-  //       Qty,
-  //       Result1,
-  //       Result2,
-  //       Result3,
-  //       Result4,
-  //       Result5,
-  //       Result6
+
 
 
 // Route to insert selected rows
@@ -88,99 +69,7 @@ const insertSelectedRows = async (req, res) => {
 };
 
 
-// const insertSelectedRows = async (req, res) => {
-//   try {
-//     const {
-//       Part_No,
-//       Item_No,
-//       OPIST_MC,
-//       OPIST_Process,
-//       Spec,
-//       Usage_Pcs,
-//       OPIST_MC_No,
-//       Qty,
-//       Result1,
-//       Result2,
-//       Result3,
-//       Result4,
-//       Result5,
-//       Result6,
-//       _Division,
-//       Dwg_Rev,
-//       Req_Case,
-//       Req_Date_Request,
-//       CT,
-//     } = req.body;
 
-//     const pool = await poolPromise;
-//     const result = await pool
-//       .request()
-
-//       .input("Part_No", Type.NVarChar, Part_No)
-//       .input("Item_No", Type.NVarChar, Item_No)
-//       .input("OPIST_MC", Type.NVarChar, OPIST_MC)
-//       .input("OPIST_Process", Type.NVarChar, OPIST_Process)
-//       .input("Spec", Type.NVarChar, Spec)
-//       .input("Usage_Pcs", Type.NVarChar, Usage_Pcs)
-//       .input("OPIST_MCNo", Type.NVarChar, OPIST_MCNo)
-//       .input("Qty", Type.NVarChar, Qty)
-//       .input("Result1", Type.NVarChar, Result1)
-//       .input("Result2", Type.NVarChar, Result2)
-//       .input("Result3", Type.NVarChar, Result3)
-//       .input("Result4", Type.NVarChar, Result4)
-//       .input("Result5", Type.NVarChar, Result5)
-//       .input("Result6", Type.NVarChar, Result6)
-//       .input("_Division", Type.NVarChar, _Division)
-//       .input("DwgRev", Type.NVarChar, DwgRev)
-//       .input("CT", Type.NVarChar, CT)
-//       .input("ReqCase", Type.NVarChar, ReqCase)
-//       .input("Req_Date_Request", Type.NVarChar, Req_Date_Request);
-
-//     result.query(
-//       "EXEC [trans].[tb_Request_Insert] @Part_No,@Item_No,@OPIST_MC,@OPIST_Process,@Spec,@Usage_Pcs,@OPIST_MC_No,@Qty,@Result1,@Result2,@Result3,@Result4,@Result5,@Result6,@_Division,@Dwg_Rev",
-//       function (err, result) {
-//         if (err) {
-//           console.log(err);
-//           res.status(500).json({ success: false, error: err.message });
-//         } else {
-//           res.json({
-//             success: true,
-//             message: "Employee added successfully",
-//             result: result,
-//           });
-//         }
-//       }
-//     );
-//   } catch (err) {
-//     console.error("Error executing query:", err.stack);
-//     res
-//       .status(500)
-//       .send({ error: "Internal Server Error", details: err.message });
-//   }
-// };
-
-
-
-
-// .input('Part_No', Type.NVarChar, Part_No)
-// .input('Item_No', Type.NVarChar, Item_No)
-// .input('OPIST_MC', Type.NVarChar, OPIST_MC)
-// .input('OPIST_Process', Type.NVarChar, OPIST_Process)
-// .input('Spec', Type.NVarChar, Spec)
-// .input('Usage_Pcs', Type.NVarChar, Usage_Pcs)
-// .input('OPIST_MC_No', Type.NVarChar, OPIST_MC_No)
-// .input('Qty', Type.NVarChar, Qty)
-// .input('Result1', Type.NVarChar, Result1)
-// .input('Result2', Type.NVarChar, Result2)
-// .input('Result3', Type.NVarChar, Result3)
-// .input('Result4', Type.NVarChar, Result4)
-// .input('Result5', Type.NVarChar, Result5)
-// .input('Result6', Type.NVarChar, Result6)
-// .input('_Division', Type.NVarChar, _Division)
-// .input('Dwg_Rev', Type.NVarChar, Dwg_Rev)
-// .input('Req_Case', Type.NVarChar, Req_Case)
-// .input('Req_Date_Request', Type.NVarChar, Req_Date_Request)
-// .input('CT', Type.NVarChar, CT)
 
 const Post_OPIST_PartNo = async function (req, res) {
   try {
@@ -282,3 +171,10 @@ module.exports = {
   Post_MC,
   Post_ToolDetial,
 };
+
+
+
+
+
+
+
