54240c7a043d073c21ccbf9fb7a5eb990c21d288
gvorn <157733779+gvorn7@users.noreply.github.com>
2024-05-30 14:21:07 +0700

    v15

--- controllers/masterRequestsController.js
+++ controllers/masterRequestsController.js
@@ -2,14 +2,10 @@ const { connectDb, closeDb, poolPromise } = require("../config/db.config");
 var Type = require("mssql").TYPES;
 
 
-
-
-// Route to insert selected rows
 const insertSelectedRows = async (req, res) => {
   try {
     const {
       Emp_Code,
-      Doc_No,
       _Division,
       Case_,
       dateOfReq,
@@ -29,6 +25,32 @@ const insertSelectedRows = async (req, res) => {
       Result5,
       Result6,
     } = req.body;
+
+    
+    const month = new Date().getMonth() + 1; 
+    let Doc_NoPrefix = '';
+
+    switch (OPIST_Process) {
+      case 'F&BORING':
+        Doc_NoPrefix = 'RL';
+        break;
+      case 'TURNING':
+        Doc_NoPrefix = 'TN';
+        break;
+      case 'MILLING':
+        Doc_NoPrefix = 'RL';
+        break;
+      case 'F&BORING1':
+        Doc_NoPrefix = 'RL';
+        break;
+      case 'F&BORING2':
+        Doc_NoPrefix = 'RL';
+        break;
+      default:
+        Doc_NoPrefix = '';
+    }
+
+    const Doc_No = `${Doc_NoPrefix}${month.toString().padStart(2, '0')}`;
 
     const pool = await poolPromise;
     const request = pool.request();
@@ -62,11 +84,82 @@ const insertSelectedRows = async (req, res) => {
     `);
 
     res.status(200).send('Rows inserted successfully');
+    // console.log(result);
   } catch (err) {
     console.error('Error inserting rows:', err);
     res.status(500).send('Error inserting rows');
   }
 };
+
+
+
+
+
+
+
+// Route to insert selected rows
+// const insertSelectedRows = async (req, res) => {
+//   try {
+//     const {
+//       Emp_Code,
+//       Doc_No,
+//       _Division,
+//       Case_,
+//       dateOfReq,
+//       OPIST_Process,
+//       ITEM_NO,
+//       OPIST_PartNo,
+//       SPEC,
+//       OPIST_Usage_pcs,
+//       Revision,
+//       OPIST_MC,
+//       MCNo,
+//       Qty,
+//       Result1,
+//       Result2,
+//       Result3,
+//       Result4,
+//       Result5,
+//       Result6,
+//     } = req.body;
+
+//     const pool = await poolPromise;
+//     const request = pool.request();
+    
+//     request.input('Emp_Code', Type.NVarChar, Emp_Code)
+//            .input('Doc_No', Type.NVarChar, Doc_No)
+//            .input('_Division', Type.NVarChar, _Division)
+//            .input('Case_', Type.NVarChar, Case_)
+//            .input('dateOfReq', Type.Date, dateOfReq)
+//            .input('OPIST_Process', Type.NVarChar, OPIST_Process)
+//            .input('ITEM_NO', Type.NVarChar, ITEM_NO)
+//            .input('OPIST_PartNo', Type.NVarChar, OPIST_PartNo)
+//            .input('SPEC', Type.NVarChar, SPEC)
+//            .input('OPIST_Usage_pcs', Type.Int, OPIST_Usage_pcs)
+//            .input('Revision', Type.NVarChar, Revision)
+//            .input('OPIST_MC', Type.NVarChar, OPIST_MC)
+//            .input('MCNo', Type.NVarChar, MCNo)
+//            .input('Qty', Type.Int, Qty)
+//            .input('Result1', Type.Int, Result1)
+//            .input('Result2', Type.Int, Result2)
+//            .input('Result3', Type.Int, Result3)
+//            .input('Result4', Type.Int, Result4)
+//            .input('Result5', Type.Int, Result5)
+//            .input('Result6', Type.Int, Result6);
+
+//     await request.query(`
+//       EXEC [trans].[stored_tb_Request_Insert] 
+//       @Emp_Code, @Doc_No, @_Division, @Case_, @dateOfReq, @OPIST_Process, 
+//       @ITEM_NO, @OPIST_PartNo, @SPEC, @OPIST_Usage_pcs, @Revision, @OPIST_MC, 
+//       @MCNo, @Qty, @Result1, @Result2, @Result3, @Result4, @Result5, @Result6
+//     `);
+
+//     res.status(200).send('Rows inserted successfully');
+//   } catch (err) {
+//     console.error('Error inserting rows:', err);
+//     res.status(500).send('Error inserting rows');
+//   }
+// };
 
 
 
